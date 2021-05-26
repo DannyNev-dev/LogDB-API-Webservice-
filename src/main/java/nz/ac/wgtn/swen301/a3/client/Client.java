4f22b72ded55d1e069f51bf932b3cd92effc2d94
Daniel Neville <nevilldani@ecs.vuw.ac.nz>
2021-05-27 02:03:47 +1200

    Implemented the client class, as well as configuring and including the maven assembly plugin to generate a packaged jar far for client

--- src/main/java/nz/ac/wgtn/swen301/a3/client/Client.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/client/Client.java
@@ -3,18 +3,63 @@
  */
 package nz.ac.wgtn.swen301.a3.client;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * @author nevilldani
  *
  */
 public class Client {
+	
+	private static final String BASE_URL = "http://localhost:8080/resthome4logs/";
 
+	
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		if(args.length!=2) {
+			throw new Error("No Parameters provided");
+		}	
+		String format = args[0];
+		String fileString = args[1];
+		Response response;
+        FileOutputStream outputStream = new FileOutputStream(fileString);
+		switch(format) {
+		case "excel":
+			response = getResponse("/statsxls");
+			ByteArrayInputStream inputStream = new ByteArrayInputStream(response.body().bytes());
+	        Workbook wb = WorkbookFactory.create(inputStream);
+	        wb.write(outputStream);
+			return;
+		case "csv":
+			response = getResponse("/statscsv");
+			String csvString = response.body().string();
+			outputStream.write(csvString.getBytes());
+			return;
+		}
+		outputStream.close();
+	}
+	
+	private static Response getResponse(String servletMap) throws IOException {
+		OkHttpClient client = new OkHttpClient();
+	    Request request = new Request.Builder()
+	    	      .url(BASE_URL + servletMap)
+	    	      .build();
 
+	    Call call = client.newCall(request);
+	    return call.execute();
 	}
 
 }
